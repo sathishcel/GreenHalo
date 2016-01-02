fc05f7d267cfdda4d7402def52ac325b114541d2
chandrasekhar <chandu.basina23@gmail.com>
2016-01-02 11:44:14 +0530

    adding wgu as a popup.

--- app/controllers/wgus_controller.rb
+++ app/controllers/wgus_controller.rb
@@ -1,2 +1,5 @@
 class WgusController < ApplicationController
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -54,8 +54,10 @@ Myapp::Application.routes.draw do
   end
 
   resources :levels
+  resources :wgus
 
   # All routes
+  post "dashboards/create" => 'dashboards#create'
   get "dashboards/dashboard_1"
   get "/getting_sub_levels/:id"  => 'dashboards#getting_sub_levels'
   get "dashboards/dashboard_2"
