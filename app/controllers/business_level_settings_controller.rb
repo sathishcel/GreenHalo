7583901e7654d65c21f588fcc8a6fb1094025eda
Vijay <Vijayakumar@celestialsys.com>
2016-01-04 19:52:22 +0530

    franchise agrement new design development

--- app/controllers/business_level_settings_controller.rb
+++ app/controllers/business_level_settings_controller.rb
@@ -1,8 +1,16 @@
 class BusinessLevelSettingsController < ApplicationController
-def index
-if request.xhr?
+  def index
+    if request.xhr?
       render partial:"system_config"
-end
-end
+    end
+  end
+
+  def terms_conditions
+    render partial: "terms_conditions"
+  end
+
+  def franchise_aggrement
+    render partial: "franchise_aggrement"
+  end
 end
 
